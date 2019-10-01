53708be9523636dc635dad9785b6cc1f904de570
Nikos Livadaris <nikosleiv@gmail.com>
2019-10-01 06:20:59 +0300

    dependencies setup

---
+++ src/gossip.cpp
@@ -0,0 +1,6 @@
+//
+// Created by nikos on 28/9/19.
+//
+
+#include "gossip.h"
+
---
+++ src/gossip.h
@@ -0,0 +1,14 @@
+//
+// Created by nikos on 28/9/19.
+//
+
+#ifndef GSPD_GOSSIP_H
+#define GSPD_GOSSIP_H
+
+
+class gossip {
+
+};
+
+
+#endif //GSPD_GOSSIP_H
