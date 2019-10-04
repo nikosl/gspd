453ac67d54a14efe2cfc3485a739c1f7041ac5ba
Nikos Livadaris <nikosleiv@gmail.com>
2019-10-04 06:16:56 +0300

    declare gossip classes header

--- src/gossip.h
+++ src/gossip.h
@@ -1,12 +1,48 @@
 #pragma once
+#include <string>
+#include <map>
+#include <vector>
 
 namespace gossip {
-    class Peer {
+enum PeerState {
+  alive,
+  zombie
+};
 
-    };
+class Peer {
+ private:
+  int64 heartbeat;
+  PeerState state;
+  std::string address;
+  std::string id;
 
-    class Members {
+ public:
+  Peer(const &
+  std::string id,
+  const &
+  std::string address
+  );
 
-    };
-} //end gossip
+  const&
+  std::string getID() const;
+  const&
+  std::string getAddress() const;
+  int64 getHeartbeat() const;
+  PEER_STATE getState()
+  void increment();
+};
 
+class Members {
+ private:
+  std::map<std::string, Peer> alive;
+  std::map<std::string, Peer> dead;
+  std::map<std::string, Peer> timers;
+ public:
+  Members();
+  bool isAlive(std::string id) const;
+  bool isDead(std::string id) const;
+  void increment(std::string id);
+  void addPeer(Peer);
+};
+
+} // namespace gossip
