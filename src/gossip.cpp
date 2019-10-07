324de59174acb344b2540b98feddfdfdfb577afd
Nikos Livadaris <nikosleiv@gmail.com>
2019-10-07 06:30:05 +0300

    code changes

--- src/gossip.cpp
+++ src/gossip.cpp
@@ -1,2 +1,7 @@
 #include "gossip.h"
 
+namespace gossip {
+Peer::Peer(std::string peer_id, std::string peer_address)
+    : _id(peer_id), _address(peer_address){};
+
+}
--- src/gossip.h
+++ src/gossip.h
@@ -1,42 +1,41 @@
 #pragma once
-#include <string>
+
 #include <map>
+#include <string>
 #include <vector>
 
 namespace gossip {
-enum PeerState {
-  alive,
-  zombie
-};
+enum PeerState { ALIVE, ZOMBIE };
 
 class Peer {
- private:
-  unsigned int heartbeat;
-  PeerState state;
-  std::string address;
-  std::string id;
+private:
+  unsigned int _heartbeat = 0;
+  PeerState _state = ALIVE;
+  std::string _address;
+  std::string _id;
 
- public:
-  Peer(std::string id, std::string address);
-
+public:
+  explicit Peer(std::string id, std::string address);
+  ~Peer();
   std::string getID() const;
   std::string getAddress() const;
   unsigned int getHeartbeat() const;
   PeerState getState() const;
   void increment();
+  void setState(PeerState st);
 };
 
 class Members {
- private:
+private:
   std::map<std::string, Peer> alive;
   std::map<std::string, Peer> dead;
   std::map<std::string, Peer> timers;
 
- public:
+public:
   Members();
-  bool isAlive(const std::string& id) const;
-  bool isDead(const std::string& id) const;
-  void increment(const std::string& id);
+  bool isAlive(const std::string &id) const;
+  bool isDead(const std::string &id) const;
+  void increment(const std::string &id);
   void addPeer(Peer);
 };
 
